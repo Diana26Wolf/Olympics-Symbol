a5c2d7620693605c1fd25da1d930b8f517a28541
Diana Malfoy <77827663+Diana26Wolf@users.noreply.github.com>
2021-04-26 08:04:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+canvas = document.getElementById("Canvas")
+ctx = canvas.getContext("2d")
+color = "blue"
+
+ctx.beginPath()
+ctx.strokeStyle = color
+ctx.lineWidth = 5
+ctx.arc(200, 210, 80, 0, 360)
+ctx.stroke()
+
+canvas = document.getElementById("Canvas")
+ctx = canvas.getContext("2d")
+color = "yellow"
+
+ctx.beginPath()
+ctx.strokeStyle = color
+ctx.lineWidth = 5
+ctx.arc(300, 270, 80, 0, 360)
+ctx.stroke()
+
+canvas = document.getElementById("Canvas")
+ctx = canvas.getContext("2d")
+color = "black"
+
+ctx.beginPath()
+ctx.strokeStyle = color
+ctx.lineWidth = 5
+ctx.arc(400, 210, 80, 0, 360)
+ctx.stroke()
+
+canvas = document.getElementById("Canvas")
+ctx = canvas.getContext("2d")
+color = "green"
+
+ctx.beginPath()
+ctx.strokeStyle = color
+ctx.lineWidth = 5
+ctx.arc(500, 270, 80, 0, 360)
+ctx.stroke()
+
+canvas = document.getElementById("Canvas")
+ctx = canvas.getContext("2d")
+color = "red"
+
+ctx.beginPath()
+ctx.strokeStyle = color
+ctx.lineWidth = 5
+ctx.arc(600, 210, 80, 0, 360)
+ctx.stroke()
